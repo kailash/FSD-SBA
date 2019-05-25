a2ec58d8d76e394402d4d80f737141e263c5fd48
Ubuntu <ubuntu@ip-172-31-45-237.ec2.internal>
2019-05-25 04:10:51 +0000

    migration/backup

---
+++ giphermanager/src/main/java/com/stackroute/rabbitmq/domain/GipherUserDTO.java
@@ -0,0 +1,16 @@
+package com.stackroute.rabbitmq.domain;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class GipherUserDTO {
+    private String username;
+    private String email;
+    private List<FavouriteGIFDTO> favouriteGIFS;
+}
